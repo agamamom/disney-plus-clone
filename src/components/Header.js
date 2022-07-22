4302a1a515ca036136d504ad88d5291db637cd60
dathoang <dathoang31082001@gmail.com>
2022-07-22 21:14:47 +0700

    init base project

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+    return (
+        <Container>
+            Header
+        </Container>
+    )
+}
+
+
+export default Header
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13;
+`;
