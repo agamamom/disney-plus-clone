9b3ce5f93310c536a35942f3448925111d9cb985
dathoang <dathoang31082001@gmail.com>
2022-07-23 10:54:04 +0700

    update base project in new branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+
 import './App.css';
-import Header from './components/Header'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
